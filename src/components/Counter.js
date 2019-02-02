a6eedbd542900cfc86754f02df968b9c72dad86e
Raushan <raushan95singh@gmail.com>
2019-02-02 12:44:40 +0530

    added scss

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,23 +1,12 @@
 import React from "react";
-import "./Style.css";
+import "./Style.scss";
 const Counter = props => {
   return (
     <div>
       <h1 className="H1">Counter: {props.count} </h1>
-      <button className="Button" onClick={props.decCount}>
-        {" "}
-        -
-      </button>
-      {"    "}
-      <button className="Button" onClick={props.reset}>
-        {" "}
-        reset{" "}
-      </button>{" "}
-      {"    "}
-      <button className="Button" onClick={props.incCount}>
-        {" "}
-        +{" "}
-      </button>
+      <button className="Button" onClick={props.decCount}> - </button>
+      <button className="Button" onClick={props.reset}> reset </button>
+      <button className="Button" onClick={props.incCount}> + </button>
     </div>
   );
 };
